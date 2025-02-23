d1ec13674551bdd9a763ee9d88b2a447d4cf6366
mglienecke <michael@glienecke.de>
2025-02-23 18:16:52 +0000

    Continuous mode

--- src/main/java/uk/ac/ed/inf/kafkasamples/StockSymbolProducer.java
+++ src/main/java/uk/ac/ed/inf/kafkasamples/StockSymbolProducer.java
@@ -4,14 +4,22 @@ package uk.ac.ed.inf.kafkasamples;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.utils.Time;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class StockSymbolProducer {
 
@@ -39,46 +47,50 @@ public class StockSymbolProducer {
 
     public final String StockSymbolsConfig = "stock.symbols";
     public final String KafkaTopicConfig = "kafka.topic";
+    public final String KafkaContinuousTopicConfig = "send.operation.continuous";
 
     private void process(String configFileName) throws IOException, InterruptedException {
         Properties kafkaPros = StockSymbolProducer.loadConfig(configFileName);
 
         var producer = new KafkaProducer<String, String>(kafkaPros);
-
         String[] symbols = ((String) kafkaPros.get(StockSymbolsConfig)).split(",");
-        Double[] currentStockValue = new Double[symbols.length];
-
-        for (int i = 0; i < symbols.length; i++) {
-            currentStockValue[i] = (double) (i + 30);
-        }
 
         int recordCount = 0;
         final String topic = kafkaPros.getProperty(KafkaTopicConfig);
+        // final String key = String.valueOf(new Random().nextInt(0, Integer.MAX_VALUE));
+        final boolean continuousMode = Boolean.parseBoolean(kafkaPros.getProperty(KafkaContinuousTopicConfig));
 
-        while (true) {
-            for (int i = 0; i < symbols.length; i++) {
-                if ((recordCount % 2) != 0){
-                    currentStockValue[i] += new Random().nextDouble();
-                } else {
-                    currentStockValue[i] -= new Random().nextDouble();
-                }
+        try {
+            int valueCounter = 1;
 
-                final String stockSymbol = symbols[i];
-                final String stockValue = currentStockValue[i].toString();
+            while (true) {
+                final String key = symbols[new Random().nextInt(symbols.length)];
+                final String value = String.valueOf(valueCounter++);
 
-                producer.send(new ProducerRecord<>(topic, stockSymbol, stockValue), (recordMetadata, ex) -> {
+                producer.send(new ProducerRecord<>(topic, key, value), (recordMetadata, ex) -> {
                     if (ex != null)
                         ex.printStackTrace();
                     else
-                        System.out.printf("Produced event to topic %s: key = %-10s value = %s%n", topic, stockSymbol, stockValue);
-                });
+                        System.out.printf("Produced event to topic %s: key = %-10s value = %s%n", topic, key, value);
+                }).get(1000, TimeUnit.MILLISECONDS);
 
-                recordCount++;
+                if (! continuousMode) {
+                    break;
+                }
+
+                Thread.sleep(100);
             }
 
-            // Thread.sleep(100);
-            System.out.println(recordCount + " records sent to Kafka");
+            System.out.println("1 record sent to Kafka");
+        } catch (ExecutionException e) {
+            System.err.println("execution exc: " + e);
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            System.err.println("timeout exc: " + e);
         }
+
+        producer.flush();
+        producer.close();
     }
 }
 
